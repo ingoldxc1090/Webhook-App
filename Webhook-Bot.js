342acee6f437eca7561360d04eeea0d7c8fcfcc4
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2020-05-25 21:35:39 -0400

    Updated process for determining new apps and error retry

--- Webhook-Bot.js
+++ Webhook-Bot.js
@@ -26,33 +26,59 @@ function httpsget(url) {	//async function for http get requests url in data out.
 }
 
 async function getList() {	//function to get and save list of all apps registered on steam
+	console.log('Checking for new Steam apps');
 	let data = JSON.parse(await httpsget("https://api.steampowered.com/ISteamApps/GetAppList/v2")).applist.apps.map(({ name, appid }) => {
 		return { cleanname: name.replace(/[^\da-zA-Z]/g, '').toLowerCase(), name, appid, apptype: undefined, developers: undefined };
 	});
 	let applist = require("./applist.json");
+	let newapps = [];
 	let i = 1
 	for(searchApp of data) {
 		process.stdout.write(`Checking app ${i} of ${data.length}`);
 		let listApp = applist.find(app => app.appid == searchApp.appid);
-		if (listApp === undefined) applist.push(searchApp);
+		if (listApp === undefined) newapps.push(searchApp);
 		i++;
 		process.stdout.write("\r\x1b[K");
 	}
+	console.log(`Found ${newapps.length} new apps`);
 	i = 1;
-	for(app of applist) {
-		process.stdout.write(`Downloading data for app ${i} of ${applist.length}`);
+	let rcount = 1;
+	let errorApps = [];
+	for(app of newapps) {
+		process.stdout.write(`Downloading data for app ${i} of ${newapps.length}`);
 		if (app.apptype === undefined) {
 			try {
 				let appdata = JSON.parse(await httpsget(`https://store.steampowered.com/api/appdetails/?appids=${app.appid}`))[app.appid.toString()];
 				if (appdata.success) app.apptype = appdata.data.type, app.developers = appdata.developers;	
 			} catch {
-				console.log(`Error downloading data for ${app.name}`);
+				//console.log(`Error downloading data for ${app.name}`);
+				errorApps.push(app)
 			}
+			rcount++;
 		}
-		if (i%10 == 0) await sleep(5000);		
+		if (rcount%10 == 0) await sleep(5000);		
 		i++;
 		process.stdout.write("\r\x1b[K");
 	}
+	i = 1;
+	let failedApps = [];
+	console.log(`Error downloading data for ${errorApps.length} apps`);
+	for(app of errorApps) {
+		process.stdout.write(`Retrying download for ${app.name} app ${i} of ${errorApps.length}`);
+		try {
+			let appdata = JSON.parse(await httpsget(`https://store.steampowered.com/api/appdetails/?appids=${app.appid}`))[app.appid.toString()];
+			if (appdata.success) {
+				app.apptype = appdata.data.type, app.developers = appdata.developers;
+				newapps.find(newapp => newapp.appid == app.id) = app;
+			}
+		} catch {
+			failedApps.push(app.name)
+		}
+		i++;
+		process.stdout.write("\r\x1b[K");
+	}
+	applist.concat(newapps);
+	if (failedApps.length > 0) console.log(`Failed to download data for: ${failedApps.toString()}`);
 	await fs.promises.writeFile("applist.json", JSON.stringify(applist));
 	await fs.promises.writeFile('gamelist.json', JSON.stringify(applist.filter(app => app.apptype === 'game')))	
 }
